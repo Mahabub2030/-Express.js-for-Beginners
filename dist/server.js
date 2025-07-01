7c0e44fba4749ddf2630b07aa7fd1cb88f3423ff
Mahaub Alam <mahabubalom72091@gmail.com>
2025-07-01 20:51:06 +0300

    hvae some isue with mongo db

--- dist/server.js
+++ dist/server.js
@@ -15,7 +15,19 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app"));
 let server;
 const port = 8000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = "mongodb://TodosApp:euBII5cHvXq2X5qI@ac-cgkxfia-shard-00-00.x9t7sgg.mongodb.net:27017,ac-cgkxfia-shard-00-01.x9t7sgg.mongodb.net:27017,ac-cgkxfia-shard-00-02.x9t7sgg.mongodb.net:27017/?ssl=true&replicaSet=atlas-nszs70-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0";
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+console.log(uri);
 const boostrap = () => __awaiter(void 0, void 0, void 0, function* () {
+    yield client.connect();
     server = app_1.default.listen(port, () => {
         console.log(`From ToDo app listening on port ${port}`);
     });
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,26 @@
 import app from "./app";
 
 let server;
-
 const port = 8000;
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+"mongodb://TodosApp:euBII5cHvXq2X5qI@ac-cgkxfia-shard-00-00.x9t7sgg.mongodb.net:27017,ac-cgkxfia-shard-00-01.x9t7sgg.mongodb.net:27017,ac-cgkxfia-shard-00-02.x9t7sgg.mongodb.net:27017/?ssl=true&replicaSet=atlas-nszs70-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+console.log(uri)
 const boostrap = async () => {
-    server = app.listen(port, () => {
+  await client.connect();
+
+  server = app.listen(port, () => {
       console.log(`From ToDo app listening on port ${port}`);
     });
 }
