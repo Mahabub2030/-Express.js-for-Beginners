b858ad2f3284bf3cdf265f700c4032830fbeb472
Mahaub Alam <mahabubalom72091@gmail.com>
2025-07-01 21:05:13 +0300

    data url added db

--- dist/server.js
+++ dist/server.js
@@ -16,7 +16,10 @@ const app_1 = __importDefault(require("./app"));
 let server;
 const port = 8000;
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri = "mongodb://TodosApp:euBII5cHvXq2X5qI@ac-cgkxfia-shard-00-00.x9t7sgg.mongodb.net:27017,ac-cgkxfia-shard-00-01.x9t7sgg.mongodb.net:27017,ac-cgkxfia-shard-00-02.x9t7sgg.mongodb.net:27017/?ssl=true&replicaSet=atlas-nszs70-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0";
+const uri = 
+// "mongodb+srv://TodosApp:h34tiiB7SVUOV5rF@cluster0.x9t7sgg.mongodb.net/todosDB?retryWrites=true&w=majority&appName=Cluster0";
+"mongodb://TodosApp:h34tiiB7SVUOV5rF@ac-cgkxfia-shard-00-00.x9t7sgg.mongodb.net:27017,ac-cgkxfia-shard-00-01.x9t7sgg.mongodb.net:27017,ac-cgkxfia-shard-00-02.x9t7sgg.mongodb.net:27017/?ssl=true&replicaSet=atlas-nszs70-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
@@ -25,9 +28,10 @@ const client = new MongoClient(uri, {
     },
 });
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-console.log(uri);
+// console.log(uri)
 const boostrap = () => __awaiter(void 0, void 0, void 0, function* () {
     yield client.connect();
+    console.log('conneted to db');
     server = app_1.default.listen(port, () => {
         console.log(`From ToDo app listening on port ${port}`);
     });
--- src/server.ts
+++ src/server.ts
@@ -5,8 +5,9 @@ const port = 8000;
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri =
-"mongodb://TodosApp:euBII5cHvXq2X5qI@ac-cgkxfia-shard-00-00.x9t7sgg.mongodb.net:27017,ac-cgkxfia-shard-00-01.x9t7sgg.mongodb.net:27017,ac-cgkxfia-shard-00-02.x9t7sgg.mongodb.net:27017/?ssl=true&replicaSet=atlas-nszs70-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0";
-
+  // "mongodb+srv://TodosApp:h34tiiB7SVUOV5rF@cluster0.x9t7sgg.mongodb.net/todosDB?retryWrites=true&w=majority&appName=Cluster0";
+  "mongodb://TodosApp:h34tiiB7SVUOV5rF@ac-cgkxfia-shard-00-00.x9t7sgg.mongodb.net:27017,ac-cgkxfia-shard-00-01.x9t7sgg.mongodb.net:27017,ac-cgkxfia-shard-00-02.x9t7sgg.mongodb.net:27017/?ssl=true&replicaSet=atlas-nszs70-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -16,9 +17,10 @@ const client = new MongoClient(uri, {
 });
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-console.log(uri)
+// console.log(uri)
 const boostrap = async () => {
   await client.connect();
+  console.log('conneted to db')
 
   server = app.listen(port, () => {
       console.log(`From ToDo app listening on port ${port}`);
